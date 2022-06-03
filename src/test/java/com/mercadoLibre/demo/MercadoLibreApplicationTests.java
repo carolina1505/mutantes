5eb39827bfc771b2d5d94ef5f3a70102f4abd66b
Carolina Cardona Garcia <ccardona@veritran.com>
2022-06-03 02:07:15 -0500

    first commit

---
+++ src/test/java/com/mercadoLibre/demo/MercadoLibreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadoLibre.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLibreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
